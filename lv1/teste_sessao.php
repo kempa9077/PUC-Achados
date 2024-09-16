133c197f993f062ef302a549d3f29d423cdc27d6
DaviMartins26 <davi.martins15@outlook.com>
2024-09-16 10:40:03 -0300

    location arrumado
    Redirecionamento de pagina quando sem sessão arrumado

--- lv1/teste_sessao.php
+++ lv1/teste_sessao.php
@@ -6,7 +6,7 @@ print_r($_SESSION);
 // Verifica se o usuário está logado
 if (!isset($_SESSION['usuario'])) {
     // Se não estiver logado, redireciona para a página de login
-    header("Location: login.html");
+    header("Location: ..//login/login.html");
     exit();}
 ?>
 
