6c616f2cb7304f62f76b31d2205b4385f131dc02
Leander-Antonio <Leander.hallu@gmail.com>
2024-09-23 10:21:27 -0300

    Atualização PagFun

--- pagFun_protocolo/pagFun_protocolo.php
+++ pagFun_protocolo/pagFun_protocolo.php
@@ -1,60 +1,33 @@
-<?php
+<?php // arrumar PHP ERRADO
+
 require_once("../funcoes_banco.php");  // Função de conexão com o banco de dados
 
 function buscar_protocolos() {
     $sql = "SELECT 
-    -- Informações do protocolo
     protocolo.idprotocolo,
     protocolo.situacao,
     DATE_FORMAT(protocolo.data_abertura, '%d/%m/%Y') AS data_abertura,
     IFNULL(DATE_FORMAT(protocolo.data_fechamento, '%d/%m/%Y'), '-') AS data_fechamento,
     DATE_FORMAT(protocolo.data_perda, '%d/%m/%Y') AS data_perda,
     protocolo.descricao,
-
-    -- Nome da pessoa que abriu o protocolo
     pessoa_abertura.nome AS nome_pessoa_abertura,
-
-    -- Nome da pessoa que fechou o protocolo (se houver)
     pessoa_fechado.nome AS nome_pessoa_fechado,
-
-    -- Bloco e sala do local_perda do protocolo
     local.bloco,
     local.sala,
-
-    -- Nome do objeto relacionado ao protocolo
     objeto.nome AS nome_objeto
-
-FROM protocolo
-
--- Join para obter o nome da pessoa que abriu o protocolo
-JOIN pessoa AS pessoa_abertura ON protocolo.pessoa_abertura = pessoa_abertura.cpf
-
--- Join para obter o nome da pessoa que fechou o protocolo (se houver)
-LEFT JOIN pessoa AS pessoa_fechado ON protocolo.pessoa_fechado = pessoa_fechado.cpf
-
--- Join para obter as informações do local (bloco e sala)
-JOIN local ON protocolo.local_perda = local.id_local
-
--- Join para obter o nome do objeto relacionado ao protocolo
-JOIN objeto ON protocolo.objeto = objeto.id_objeto
-";
+    FROM protocolo
+    JOIN pessoa AS pessoa_abertura ON protocolo.pessoa_abertura = pessoa_abertura.cpf
+    LEFT JOIN pessoa AS pessoa_fechado ON protocolo.pessoa_fechado = pessoa_fechado.cpf
+    JOIN local ON protocolo.local_perda = local.id_local
+    JOIN objeto ON protocolo.objeto = objeto.id_objeto";
 
     $result = consultar_dado($sql);
     echo json_encode($result);
-
 }
-// provavelmente ta errado
-if (isset($_GET['acao'])) {
-    $acao = $_GET['acao'];
 
-    switch ($acao) {
-        case 'buscar':
-            buscar_protocolos();
-            break;
-        default:
-            echo "Ação inválida.";
-    }
+if (isset($_GET['acao']) && $_GET['acao'] == 'buscar') {
+    buscar_protocolos();
 } else {
-    echo "Nenhuma ação especificada.";
+    echo json_encode(["erro" => "Ação inválida."]);
 }
 ?>
